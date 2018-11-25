81dcb253342d90d2e3e21042d1cbceeb0a22b543
gocho <gocho@ebuyclub.com>
2018-11-25 11:42:51 +0100

    add timestamp

--- avrojsonconverter/src/main/java/testConverter.java
+++ avrojsonconverter/src/main/java/testConverter.java
@@ -7,27 +7,35 @@ import java.io.UnsupportedEncodingException;
 
 
 public class testConverter {
+
+    public testConverter(){}
+
     public static void main (String[] args) throws UnsupportedEncodingException {
         JsonAvroConverter converter = new JsonAvroConverter();
+        testConverter test = new testConverter();
         String schema =
                 "{" +
                         "   \"type\" : \"record\"," +
                         "   \"name\" : \"Acme\"," +
-                        "   \"fields\" : [{ \"name\" : \"username\", \"type\" : \"string\" }, { \"name\" : \"age\", \"type\":\"int\"}]" +
+                        "   \"fields\" : [{ \"name\" : \"username\", \"type\" : \"string\" }, { \"name\" : \"age\", \"type\":\"int\"}, { \"name\" : \"date\", \"type\" : [\"null\", {" +
+                        "            \"type\" : \"long\"," +
+                        "            \"logicalType\" : \"timestamp-millis\"}]}]" +
                         "}";
-        GenericRecord test = generateGenericRecord("guillaume", 33, schema);
-        byte[] bytes = converter.convertToJson(test);
-        System.out.println(new String(bytes, "UTF-8"));
+        GenericRecord gr = test.generateGenericRecord("guillaume", 33, System.currentTimeMillis(), schema);
+        byte[] bytes = converter.convertToJson(gr);
+        String jsonString = new String(bytes, "UTF-8");
+        System.out.println(jsonString);
 
 
     }
 
-    static GenericRecord generateGenericRecord(String name, int age, String schema) {
+    private GenericRecord generateGenericRecord(String name, int age, Long date, String schema) {
         Schema.Parser parser = new Schema.Parser();
         Schema schemaParsed = parser.parse(schema);
         GenericRecord gr = new GenericData.Record(schemaParsed);
         gr.put("username", name);
         gr.put("age", age);
+        gr.put("date", date);
         return gr;
     }
 
